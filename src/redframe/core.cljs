cd45ce91bfec324dc109b5cc891c0c18c46158b9
Tom.Yu <569721086@qq.com>
2020-11-19 18:55:30 +0800

    add <!

--- src/redframe/core.cljs
+++ src/redframe/core.cljs
@@ -1,6 +1,8 @@
 (ns redframe.core
   (:require
-   [redframe.model :as m])
+   [redframe.model :as m]
+   [cljs.core.async :as async])
   (:require-macros redframe.core))
 
 (def reg-model m/reg-model)
+(def <! async/<!)
